889e692e032bdea40144bf8d8c17ee2dcf81876d
Arthri <41360489+Arthri@users.noreply.github.com>
2022-04-28 19:44:20 +0800

    revert: "Remove ban conversion command as it now happens automatically"
    This reverts commit d68b1ed5adfde5c8992acf0985d1ca2d2637f1e1.

--- TShockAPI/Commands.cs
+++ TShockAPI/Commands.cs
@@ -1320,6 +1320,9 @@ namespace TShockAPI
 			//						Displays a paginated list of bans
 			// ban details <ban ID>
 			//						Target is expected to be a ban Unique ID
+			// ban convert
+			// ban-convert-confirm
+			//						Converts all old bans to new ban system.
 
 			void Help()
 			{
@@ -1625,6 +1628,19 @@ namespace TShockAPI
 
 				DisplayBanDetails(ban);
 			}
+
+			void ConvertBans()
+			{
+				args.Player.SendWarningMessage("This will convert all bans from the old ban system to the new identifier-based bans.");
+				args.Player.SendWarningMessage($"If you are sure you wish to proceed, please execute {"ban-convert-confirm".Color(Utils.WhiteHighlight)} to continue.");
+				args.Player.AddResponse("ban-convert-confirm", (obj) =>
+				{
+					TShock.Bans.TryConvertBans();
+
+					var cmdArgs = (CommandArgs)obj;
+					cmdArgs.Player.SendSuccessMessage("Bans have been converted.");
+				});
+			}
 			
 			string subcmd = args.Parameters.Count == 0 ? "help" : args.Parameters[0].ToLower();
 			switch (subcmd)
@@ -1647,6 +1663,10 @@ namespace TShockAPI
 
 				case "details":
 					BanDetails();
+					break;
+
+				case "convert":
+					ConvertBans();
 					break;
 
 				default:
